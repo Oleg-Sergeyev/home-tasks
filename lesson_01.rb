d500425c6f0c2b0f1a2c563afb4d0a2e6a51e07f
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-06-03 14:26:22 +0300

    Changed lesson 01

--- lesson_01.rb
+++ lesson_01.rb
@@ -3,13 +3,15 @@ require 'date'
 puts 'RUBY VERSION is ' + RUBY_VERSION
  
 #Второе и третье задание
-_current_year = (Date.today).year
-_current_month = (Date.today).month
-_current_day = (Date.today).day
-_currnet_date = Date.today
-#Переменные для проверки на валидность по календарю ()
-_year_birthday = ''
-_birthday_date = ''
+
+#Текущий год и текущая дата 
+current_year = (Date.today).year
+currnet_date = Date.today
+#_current_month = (Date.today).month #_current_day = (Date.today).day
+
+#Переменные учавствующие в функциях
+year_birthday = ''
+birthday_date = ''
     
 #Массив для перевода названия месяца
 RU_MONTHS = {'Jan' => 'Января', 'Feb' => 'Февраля', 
@@ -22,36 +24,36 @@ def eng_to_russian_date(date_string)
     date_string.gsub(/Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec/, RU_MONTHS)
 end
 
-puts 'Сегодня: ' + eng_to_russian_date(_currnet_date.strftime('%d %b %Y'))
+puts 'Сегодня: ' + eng_to_russian_date(currnet_date.strftime('%d %b %Y'))
 puts 'Привет, Олег!'
 
 #Цикл ввода даты рождения
 loop do 
   print "Напиши дату рождения, в виде yyyy-mm-dd или 'q' для выхода: "
-  _birthday_date = gets.strip
-    if _birthday_date.to_s == "q" #Если введено 'q' выход из программы
+  birthday_date = gets.strip
+    if birthday_date.to_s == "q" #Если введено 'q' выход из программы
       abort "Пока, пока!"
     end
   #Получаем из введеной даты год, месяц, день
-  _year_birthday, _month_birthday, _day_birthday = _birthday_date.split('-')
+  year_birthday, _month_birthday, _day_birthday = birthday_date.split('-')
   #Пока дата невалидна продолжаем ввод
-  break if Date.valid_date?(_year_birthday.to_i,_month_birthday.to_i,_day_birthday.to_i)
+  break if Date.valid_date?(year_birthday.to_i,_month_birthday.to_i,_day_birthday.to_i)
   puts 'Дата некорректна!' 
 end 
 #Вычисление возраста
-_total_old =  _current_year -_year_birthday.to_i
+total_old =  current_year - year_birthday.to_i
         
 #Формирование строки для вывода дня рождения в формате "хх ххххххх хххх"
-_str_birthday = eng_to_russian_date(Date.parse(_birthday_date).strftime('%d %b %Y'))
+str_birthday = eng_to_russian_date(Date.parse(birthday_date).strftime('%d %b %Y'))
 
 #Функция вывода сообщения возраста в зависимости от последней цифры и др.
-def chk_last_num_years_old(total_old, str_birthday)
+def chk_last_num_years_old(total_old, str_bd)
   if (total_old.to_s[1]).to_i < 5 && (total_old.to_s[1]).to_i >= 1
-    puts 'Тебе: ' + total_old.to_s + ' года и твой день рождения ' + str_birthday
-    else  puts 'Тебе: ' + total_old.to_s + ' лет и твой день рождения ' + str_birthday
+    puts 'Тебе: ' + total_old.to_s + ' года и твой день рождения ' + str_bd
+    else  puts 'Тебе: ' + total_old.to_s + ' лет и твой день рождения ' + str_bd
   end
 end
-chk_last_num_years_old(_total_old, _str_birthday)
+chk_last_num_years_old(total_old, str_birthday)
 
     
 
