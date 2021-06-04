509c6ef9695e0aa6c3ebfa38bb1623618aec365b
sergeyev_oleg <sergeyev_ov@mail.ru>
2021-06-04 22:55:11 +0300

    Changes code

--- lesson_2/ClassLocalUser.rb
+++ lesson_2/ClassLocalUser.rb
@@ -1,41 +1,42 @@
 class LocalUser
   require 'digest'
+  require 'date'
   require 'io/console'
   require 'csv'
 
   @user_name = ''
   @psw_hash = ''
   @psw1 = ''
-  @local_path = Dir.pwd
-  @file_name = "users.csv"
+  @local_path = ''
+  @file_name = ''
   
   def create_user 
     loop do
       print "Create new user, please set user name or 'q' for exit: "
       @user_name = gets.strip
-      chk_exit_prog(@user_name)
+      chk_manage_symbols('input_data', @user_name)
       break if !(@user_name.length<3 && @user_name.length<10)
       puts 'Name must have minimum 3 and maximum 10 symbols'
     end
     loop do
       @psw1 = IO::console.getpass "Create new password: "
-      chk_exit_prog(@psw1)
+      chk_manage_symbols('input_data', @psw1)
       break if (@psw1.length <= 10 && @psw1.length >= 6)
       puts 'Name must have minimum 6 and maximum 10 symbols'
     end
     loop do
       psw2 = IO::console.getpass "Repeat password or 'q' for exit: "
-      chk_exit_prog(psw2)
+      chk_manage_symbols('input_data', psw2)
       break if @psw1 == psw2
       puts 'Password mismatch!'
     end
     @psw_hash = Digest::MD5.hexdigest @psw1
-    #puts @psw_hash
-  end
-  def chk_exit_prog(str)
-    if str == "q" # Если введено 'q' выход из программы
-      abort 'Bye, bye!'
+    print "Write new user 'y' - yes, 'n' - no ?"
+    if chk_manage_symbols('write_file', gets.strip) == true
+    puts 'user ' + @user_name + ' saved'
+    #abort 'Bye, bye!'
     end
+    return true
   end
   def show_user
     if @psw_hash == '' 
@@ -45,17 +46,26 @@ class LocalUser
       puts 'User name: ' + @user_name + "\n" + _str
   end
   def write_file
-    @local_path = Dir.pwd
+    @local_path = File.expand_path(File.dirname(__FILE__))
     @file_name = 'users.csv'
-    puts @local_path
-    puts @file_name
-    if !File.exists?(@local_path + @file_name)
-      abort "File not exist!"
+    _file = @local_path + '/' + @file_name
+    if !File.exists?(_file)
+      _type_open = "w"
+    else 
+      _type_open = "a"
     end
-    CSV.open(@local_path, "a") do |csv|
-      csv << ["1", "user", "pass", "ok"]
-    #else puts 'File not exist'
+    CSV.open(_file, _type_open) do |csv|
+      csv << [@user_name, @psw_hash, Date.today]
     end
-    #File.open('users.csv', "w+")
+  end
+  def chk_manage_symbols(str1, str2)
+    if str1 == "write_file" && str2 == "n" || str1 == "input_data" && str2 == "q"
+      abort 'Bye, bye!'
+      return false
+    end
+    if str1 == "write_file" && str2 == "y"
+      write_file()
+      return true
+    end
   end
 end
--- lesson_2/run.rb
+++ lesson_2/run.rb
@@ -1,6 +1,12 @@
 require_relative 'ClassLocalUser.rb'
 
 user = LocalUser.new
-#user.create_user
+
+if user.create_user == true
+  print "Do you want manage any users ? 'y/n': "
+  if gets.strip == "n"
+    abort "Bye, bye!"
+  end
+end
 #user.show_user
-user.write_file
+#user.write_file
