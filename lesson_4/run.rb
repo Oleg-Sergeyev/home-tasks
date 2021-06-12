f6941935aa2c6b83d92d18cfa643052b7d74a467
sergeyev_oleg <sergeyev_ov@mail.ru>
2021-06-13 00:02:35 +0300

    add some code

--- lesson_4/run.rb
+++ lesson_4/run.rb
@@ -1,36 +1,53 @@
-#
-#info = `lspci -v | grep Audio`
-#info = `lspci -v`
-#puts info.class
-#file = File.new('info.txt', 'w')
-#file.write(info)
-#info.each_line(chomp: true) { |string|
-#puts string.split('\n').first
-#}
-#info.each_line(chomp: true) { |string|
-#print string[1]
-#}
-#File.open('logfile.txt', 'r').each do |string|
- #puts string.split('\n').first
-# print string
-#end
-file_path = "/home/oleg/ruby/repo/gb/home-tasks/lesson_4/logfile.txt"
-f = File.new(file_path, 'r:UTF-8')
-lines = f.readlines
+require 'json'
 
+PATH = __dir__.to_s + '/tmp'.to_s
+FILE_LOG = PATH + '/hardware.log'.freeze
+FILE_JSON = PATH + '/export.json'.freeze
+if Dir.exist?(PATH) == false
+  Dir.mkdir(PATH)
+else
+   if File.exist?(FILE_LOG)
+     File.delete(FILE_LOG)
+   end
+   if File.exist?(FILE_JSON)
+    File.delete(FILE_JSON)
+   end
+end
 
-#puts lines.sample
-#puts lines[0].lines(':')[1]
+file_log = File.new(FILE_LOG, 'w')
+file_log.write(`lspci -v`)
+file_log.close
+
+file_log = File.new(FILE_LOG, 'r:UTF-8')
+lines = file_log.readlines
+
+main_arr = []
+param_arr = []
+dev = ''
 lines.each do |string|
-    string = string.gsub /^$\n/, ''
-    string.each_line do |str|
-    #print str[0]
-      if !str.start_with?("\t") || !str.start_with?("")
-        #puts str.gsub(str[0..7],"")
-        puts str.gsub(str[0..7],"").lines(':')[0].chop.gsub(" ", "_").downcase
-        #puts str.join('_')
-      else
-        puts str.gsub(str[0],"").lines(':')[0].chop.gsub(" ", "_").downcase
+  string = string.gsub /^$\n/, ''
+  string.each_line do |str|
+    if !str.start_with?("\t") || !str.start_with?('')
+      if param_arr.empty? != true
+        main_arr.push(dev => param_arr)
+        param_arr = []
       end
+      dev = str[0..7].rstrip!
+      param_arr.push(
+        str.gsub(str[0..7], '').lines(':')[0].chop.gsub(' ', '_').downcase =>
+        str.gsub(str[0..7], '').lines(':')[1].chop.lstrip!.downcase
+      )
+      else
+        if str.gsub(str[0], '').lines(':')[1].nil? == false
+          param_arr.push(
+            str.gsub(str[0], '').lines(':')[0].chop.gsub(' ', '_').downcase =>
+            str.gsub(str[0], '').lines(':')[1].chop.lstrip!.downcase
+          )
+          else
+            param_arr.push(str.gsub(str[0], '').chop => 'NA')
+        end
+    end
   end
 end
+File.open(FILE_JSON, 'wb') { |json_file| json_file.puts JSON.pretty_generate(main_arr) }
+File.delete(FILE_LOG)
