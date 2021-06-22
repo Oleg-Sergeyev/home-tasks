3ba98a33165fdb83f8da2858e18b95c927125f8f
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-06-22 17:09:04 +0300

    changed code

--- lesson_6/lib/hello.rb
+++ lesson_6/lib/hello.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require 'date'
+require 'time'
 
 class Hello
-  MORNING = (DateTime.parse('06:00')..DateTime.parse('11:59')).freeze
-  DAY = (DateTime.parse('12:00')..DateTime.parse('17:59')).freeze
-  EVENING = (DateTime.parse('18:00')..DateTime.parse('23:59')).freeze
-  NIGHT = (DateTime.parse('00:01')..DateTime.parse('05:59')).freeze
+  MORNING = (Time.parse('06:00:00')..Time.parse('11:59:59')).freeze
+  DAY = (Time.parse('12:00:00')..Time.parse('17:59:59')).freeze
+  EVENING = (Time.parse('18:00:00')..Time.parse('23:59:59')).freeze
+  NIGHT = (Time.parse('00:00:00')..Time.parse('05:59:59')).freeze
 
   def greeting
-    return 'Доброе утро' if MORNING.cover?(DateTime.now)
-    return 'Добрый день' if DAY.cover?(DateTime.now)
-    return 'Добрый вечер' if EVENING.cover?(DateTime.now)
-    return 'Добрый ночи' if NIGHT.cover?(DateTime.now)
+    return "\nДоброе утро!" if MORNING.cover?(Time.now)
+    return "\nДобрый день!" if DAY.cover?(Time.now)
+    return "\nДобрый вечер!" if EVENING.cover?(Time.now)
+    return "\nДобрый ночи!" if NIGHT.cover?(Time.now)
   end
 end
