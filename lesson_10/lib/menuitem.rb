3f2b2209b8cb456bb4db0eb3719abe3f33a8a89f
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-07-01 14:25:11 +0300

    created folders and files for Lesson_10

---
+++ lesson_10/lib/menuitem.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'io/console'
+
+# Class MenuItem
+class MenuItem
+  attr_accessor :next_task
+
+  def initialize(array)
+    _, _, @task = *array
+    HomeWork.new(*array)
+    print MESSAGE_PRESS_KEY
+    @next_task = next_task
+    presskey
+  end
+
+  def presskey
+    input = $stdin.getch
+    presskey unless OPERATING_SYMBOLS.key?(input)
+
+    symbol = OPERATING_SYMBOLS[input]
+    menu(symbol) if symbol
+  end
+
+  def menu(symbol)
+    case symbol
+    when :start_over
+      @next_task = @task
+    when :next
+      @next_task = @task + 1
+    when :back
+      @next_task = @task - 1
+    when :quit
+      abort THANKS
+    end
+  end
+end
