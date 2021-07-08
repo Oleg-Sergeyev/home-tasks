d0a10b4696c57d5f230c8d0bfd7ad851d832c079
Sergeyev-Oleg <sergeyev_ov@mail.ru>
2021-07-08 15:14:44 +0300

    changed code

---
+++ course_work/lib/calendar.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'date'
+
+# Class Calendar
+class Calendar
+  attr_reader :weeks
+
+  def initialize(year, month)
+    @arr_dates = dates_in_month(year, month)
+    @mondays = []
+    @tuesdays = []
+    @wednesdays = []
+    @thursdays = []
+    @fridays = []
+    @saturdays = []
+    @sundays = []
+    @weeks = []
+    full_array
+  end
+
+  def dates_in_month(year, month)
+    first_day = Date.new(year, month, 1)
+    last_day = Date.new(year, month, 31)
+    @arr_dates = (first_day..last_day).to_a
+  end
+
+  def full_array
+    @arr_dates.each { |date| days(date) }
+    @weeks = { 'ПН' => @mondays,
+               'ВТ' => @tuesdays,
+               'СР' => @wednesdays,
+               'ЧТ' => @thursdays,
+               'ПТ' => @fridays,
+               'СБ' => @saturdays,
+               'ВС' => @sundays }
+  end
+
+  def days(date)
+    @mondays.push(date) if date.monday?
+    @tuesdays.push(date) if date.tuesday?
+    @wednesdays.push(date) if date.wednesday?
+    @thursdays.push(date) if date.thursday?
+    @fridays.push(date) if date.friday?
+    @saturdays.push(date) if date.saturday?
+    @sundays.push(date) if date.sunday?
+  end
+end
--- course_work/run.rb
+++ course_work/run.rb
@@ -1,51 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'lib/calendar'
 require 'date'
 
-#p Date.today.strftime("%^B")
-#p Date.today.mday
+month = Calendar.new(2021, 7).weeks
 
-def days_in_month(year, month)
-  Date.new(year, month, -1).day
+def chk_dates(dates)
+  return 4 if dates.size == 4
 end
 
-#last_day = days_in_month(Date.today.year, Date.today.month)
-
-first_day = Date.new(2021, 7, 1)
-last_day = Date.new(2021, 7, 31)
-arr_days = (first_day..last_day).to_a
-# print "\nПонедельник "
-# arr_days.select(&:monday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nВторник "
-# arr_days.select(&:tuesday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nСреда "
-# arr_days.select(&:wednesday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nЧетверг "
-# arr_days.select(&:thursday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nПятница "
-# arr_days.select(&:friday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nСуббота "
-# arr_days.select(&:saturday?).each { |date| print " #{date.strftime('%d')}" }
-# print "\nВоскресенье "
-# arr_days.select(&:sunday?).each { |date| print " #{date.strftime('%d')}" }
-mondays = []
-tuesdays = []
-wednesdays = []
-thursdays = []
-fridays = []
-saturdays = []
-sundays = []
-month = []
-arr_days.each do |date|
-  mondays.push(date) if date.monday?
-  tuesdays.push(date) if date.tuesday?
-  wednesdays.push(date) if date.wednesday?
-  thursdays.push(date) if date.thursday?
-  fridays.push(date) if date.friday?
-  saturdays.push(date) if date.saturday?
-  sundays.push(date) if date.sunday?
+def chk(day, index)
+  return 4 if day.strftime('%d').to_i - index == 4 && day.strftime('%d').to_i <= 7
 end
-month = [mondays, tuesdays, wednesdays, thursdays, fridays, saturdays, sundays]
 
-month.each { |arr| arr }.each { |day| print " #{day.strftime('%d')}"}
-
+month.each_with_index do |(name, dates), index|
+  print "\n#{name}: "
+  dates.each_with_index { |day, _ind| print format("%#{chk(day, index + 1)}d ", day.strftime('%d').to_i) }
+end
