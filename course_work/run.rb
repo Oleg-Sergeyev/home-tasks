68fbfbaf67f1f3915fb9650ff46743cf878354ff
Sergeyev-Oleg <sergeyev_ov@mail.ru>
2021-07-08 18:15:35 +0300

    changed code

--- course_work/run.rb
+++ course_work/run.rb
@@ -3,13 +3,28 @@
 require_relative 'lib/calendar'
 require 'date'
 
-month = Calendar.new(2021, 7).weeks
+month = Calendar.new(2021, 5).weeks
 
-def chk(day, index)
-  return 4 if day.strftime('%d').to_i - index == 4 && day.strftime('%d').to_i <= 7
+def chk(size, day, index)
+  #p day.strftime('%d').to_i
+  if day.strftime('%d').to_i - index <= 4 && day.strftime('%d').to_i <= 7 #|| day.strftime('%d').to_i < 5 && index == 1
+    #puts "debug:#{day.strftime('%d').to_i}"
+    #p day.strftime('%d').to_i - index <= 4 && day.strftime('%d').to_i <= 7
+    return 3
+  else
+    return 0
+  end
+end
+
+def int(day)
+  if day.strftime('%d').to_i < 10
+    return 2
+  else
+    return 0
+  end
 end
 
 month.each_with_index do |(name, dates), index|
-  print "\n#{name}: "
-  dates.each_with_index { |day, _ind| print format("%#{chk(day, index + 1)}d ", day.strftime('%d').to_i) }
+  print "\n#{name}:"
+  dates.each_with_index { |day, _ind| print format("%#{chk(dates.size, day, index + 1) + int(day)}d ", day.strftime('%d').to_i) }
 end
