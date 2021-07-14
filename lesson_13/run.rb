96d321a08987e2d5f021bf826379e73189ae1254
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-07-15 02:33:00 +0300

    added_some_code to_task8

--- lesson_13/run.rb
+++ lesson_13/run.rb
@@ -5,35 +5,36 @@ require 'time'
 puts `clear`
 # Task 1
 # class Building
-#   attr_accessor :floor, :entrance, :flat, :elevator, :max_price, :min_price
+class Building
+  attr_accessor :floor, :entrance, :flat, :elevator, :max_price, :min_price
 
-#   ELEVATORS_ENTRANCE = 2
-#   FLATS_FLOOR = 5
+  ELEVATORS_ENTRANCE = 2
+  FLATS_FLOOR = 5
 
-#   def initialize
-#     @entrance = 2
-#     @max_price = max_price
-#     @min_price = min_price
-#   end
+  def initialize
+    @entrance = 2
+    @max_price = max_price
+    @min_price = min_price
+  end
 
-#   def build_floor(floor)
-#     @elevator = @entrance * ELEVATORS_ENTRANCE
-#     @flat = floor * entrance * FLATS_FLOOR
-#   end
+  def build_floor(floor)
+    elevator = entrance * ELEVATORS_ENTRANCE
+    flat = floor * entrance * FLATS_FLOOR
+  end
 
-#   def everage_cost
-#     (max_price + min_price) / 2
-#   end
-# end
+  def everage_cost
+    (max_price + min_price) / 2
+  end
+end
 
-# print "\nВведите количество этажей предполагаемого дома: "
-# floors = gets.chomp.to_i
-# building1 = Building.new
-# building1.build_floor(floors)
-# building1.max_price = 6_000_000
-# building1.min_price = 16_000_000
-# puts "\nПо нормативам построили дом: #{building1.flat} квартир и #{building1.elevator} лифта."
-# puts "\nСредняя стоимость кватиры: #{building1.everage_cost}"
+print "\nВведите количество этажей предполагаемого дома: "
+floors = gets.chomp.to_i
+building1 = Building.new
+building1.build_floor(floors)
+building1.max_price = 6_000_000
+building1.min_price = 16_000_000
+puts "\nПо нормативам построили дом: #{building1.flat} квартир и #{building1.elevator} лифта."
+puts "\nСредняя стоимость кватиры: #{building1.everage_cost}"
 
 # Task 2
 # class Chess
@@ -117,136 +118,141 @@ puts `clear`
 
 # game = Chess.new(:white, :classic)
 # game.move(Chess::Pown, [1, 0], [1, 2])
+
 # Task 3
 # class Week
-#   attr_reader :nmdays
+class Week
+  attr_reader :nmdays
 
-#   DAYS = %w[Понедельник Вторник Среда Четверг Пятница Суббота Воскресенье].freeze
-#   def initialize
-#     @nmdays = DAYS
-#   end
+  DAYS = %w[Понедельник Вторник Среда Четверг Пятница Суббота Воскресенье].freeze
+  def initialize
+    @nmdays = DAYS
+  end
 
-#   def days(&block)
-#     return unless block_given?
+  def days(&block)
+    return unless block_given?
 
-#     nmdays.each(&block)
-#   end
+    nmdays.each(&block)
+  end
 
-#   def days_map(&block)
-#     new_array = []
-#     nmdays.each do |element|
-#       new_array.push(block.call(element))
-#     end
-#     new_array
-#   end
+  def days_map(&block)
+    new_array = []
+    nmdays.each do |element|
+      new_array.push(block.call(element))
+    end
+    new_array
+  end
 
-#   def days_select(&block)
-#     new_array = []
-#     nmdays.each do |element|
-#       res = block.call(element)
-#       new_array.push(element) if res
-#     end
-#     new_array
-#   end
-# end
+  def days_select(&block)
+    new_array = []
+    nmdays.each do |element|
+      res = block.call(element)
+      new_array.push(element) if res
+    end
+    new_array
+  end
+end
 
-# week = Week.new
-# week.days(&:puts) #{ |x| p x }
+week = Week.new
+week.days { |x| p x }
 
-# new_arr = week.days_map { |x| "[#{x}]" }
-# p new_arr
+new_arr = week.days_map { |x| "[#{x}]" }
+p new_arr
 
-# p week.days_select { |v| v.downcase.include?('ник') }
+p week.days_select { |v| v.downcase.include?('ник') }
 
 # Task 4
 # class Factory
-# class Factory
-#   attr_accessor :arr
+class Factory
+  attr_accessor :arr
 
-#   def initialize
-#     @arr = []
-#   end
+  def initialize
+    @arr = []
+  end
 
-#   def build(toy)
-#     case toy
-#     when :teddy_bear then arr.push(teddy_bear: TeddyBear.new)
-#     when :ball then arr.push(ball: Ball.new)
-#     when :cube then arr.push(cube: Cube.new)
-#     end
-#   end
+  def build(toy)
+    case toy
+    when :teddy_bear then arr.push(teddy_bear: TeddyBear.new)
+    when :ball then arr.push(ball: Ball.new)
+    when :cube then arr.push(cube: Cube.new)
+    end
+  end
 
-#   def total
-#     arr.count
-#   end
+  def total
+    arr.count
+  end
 
-#   def offers
-#     toys = Hash.new(0)
-#     arr.each do |hash|
-#       hash.each do |name, _object|
-#         toys[name] += 1
-#       end
-#     end
-#     toys
-#   end
+  def offers
+    toys = Hash.new(0)
+    arr.each do |hash|
+      hash.each do |name, _object|
+        toys[name] += 1
+      end
+    end
+    toys
+  end
 
-#   class TeddyBear
-#   end
+  class TeddyBear
+  end
 
-#   class Ball
-#   end
+  class Ball
+  end
 
-#   class Cube
-#   end
-# end
+  class Cube
+  end
+end
 
-# factory = Factory.new
-# 4.times { factory.build(:teddy_bear) }
-# 5.times { factory.build(:ball) }
-# 3.times { factory.build(:cube) }
+factory = Factory.new
+4.times { factory.build(:teddy_bear) }
+5.times { factory.build(:ball) }
+3.times { factory.build(:cube) }
 
-# puts "\nВсего произведено #{factory.total} игрушек"
-# print "\nИз них: "
-# factory.offers.each { |key, value| print "\n#{key} = #{value}" }
-# puts "\n"
+puts "\nВсего произведено #{factory.total} игрушек"
+print "\nИз них: "
+factory.offers.each { |key, value| print "\n#{key} = #{value}" }
+puts "\n"
 
 # Task 5
 # class Time
-# class Time
-#   MORNING = (Time.parse('06:00:00')..Time.parse('11:59:59')).freeze
-#   DAY = (Time.parse('12:00:00')..Time.parse('17:59:59')).freeze
-#   EVENING = (Time.parse('18:00:00')..Time.parse('23:59:59')).freeze
-#   NIGHT = (Time.parse('00:00:00')..Time.parse('05:59:59')).freeze
+class Time
+  MORNING = (Time.parse('06:00:00')..Time.parse('11:59:59')).freeze
+  DAY = (Time.parse('12:00:00')..Time.parse('17:59:59')).freeze
+  EVENING = (Time.parse('18:00:00')..Time.parse('23:59:59')).freeze
+  NIGHT = (Time.parse('00:00:00')..Time.parse('05:59:59')).freeze
 
-#   def hello
-#     return "\nДоброе утро!" if MORNING.cover?(Time.now)
-#     return "\nДобрый день!" if DAY.cover?(Time.now)
-#     return "\nДобрый вечер!" if EVENING.cover?(Time.now)
-#     return "\nДобрый ночи!" if NIGHT.cover?(Time.now)
-#   end
-# end
+  def hello
+    return "\nДоброе утро!" if MORNING.cover?(Time.now)
+    return "\nДобрый день!" if DAY.cover?(Time.now)
+    return "\nДобрый вечер!" if EVENING.cover?(Time.now)
+    return "\nДобрый ночи!" if NIGHT.cover?(Time.now)
+  end
+end
 
-# time = Time.new
-# puts time.hello
+time = Time.new
+puts time.hello
 
 # Task 6
 # class Integer
-# class Integer
-#   def minutes
-#     self * 60
-#   end
+class Integer
+  MSEC = 60
+  HSEC = 3_600
+  DSEC = 86_400
+  def minutes
+    self * MSEC
+  end
 
-#   def hours
-#     self * 3_600
-#   end
+  def hours
+    self * HSEC
+  end
 
-#   def days
-#     self * 86_400
-#   end
-# end
+  def days
+    self * DSEC
+  end
+end
 
-# puts "#{5.minutes} сек."
-# puts "#{5.hours} сек."
-# puts "#{5.days} сек."
+puts "#{5.minutes} сек."
+puts "#{5.hours} сек."
+puts "#{5.days} сек."
 
 # Task 7
 # class Substace
@@ -278,9 +284,9 @@ class Substance
   end
 
   def status(act)
-    STATES.each do |key, arr|
+    STATES.each do |sym, arr|
       arr.each do |value|
-        return RU.key(key) if value == act
+        return RU.key(sym) if value == act
       end
     end
   end
@@ -290,3 +296,28 @@ water = Substance.new
 puts "Текущее состояние вещества '#{water.freeze}'"
 ice = Substance.new
 puts "Текущее состояние вещества '#{ice.melt}'"
+# Task 8
+# class Substance
+#   state_machine :state, :initial => :sub_state do
+#     before_transition :on => :some_event1, :do => :method1
+#     after_transition :on => :some_event2, :do => :method2
+#   end
+#   event :some_event1 do
+#     transition [] => :sub_state1
+#   end
+#   event :some_event2 do
+#     transition [:yyy, :xxx] => :sub_state2
+#   end
+
+#   state :sub_state do
+#     def some
+#       0
+#     end
+#   end
+
+#   state :xxx, :yyy do
+#     def some
+#       0
+#     end
+#   end
+# end
