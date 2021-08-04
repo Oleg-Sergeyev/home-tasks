82b59ab3f914a2c183d0558592b0dea41aef908b
Sergeyev-Oleg <sergeyev_ov@mail.ru>
2021-08-04 18:49:56 +0300

    changed some code

--- Lesson_19/lib/randomtext.rb
+++ Lesson_19/lib/randomtext.rb
@@ -11,18 +11,29 @@ class RandomText
   LS = ".\r\n"
   def initialize(text_size)
     @text_size = text_size
+    @arr_text = []
     @text = gettext
+    
   end
 
   def gettext
-    arr_text = []
     rand(MIN..MAX_STRING).times do
       line_string = string
-      return arr_text.join unless (line_string.bytesize + arr_text.join.bytesize) < text_size
+      return @arr_text.join unless (line_string.bytesize + @arr_text.join.bytesize) < text_size
 
-      arr_text.push(line_string)
+      @arr_text.push(line_string)
+      #puts "DEBUG1_before: #{text_size - @arr_text.join.bytesize}"
     end
-    arr_text.join
+    #puts text_size / 10
+    if (text_size - @arr_text.join.bytesize) > 100
+      puts "DEBUG_before: #{text_size - @arr_text.join.bytesize}"
+      gettext
+      puts "DEBUG_after: #{text_size - @arr_text.join.bytesize}"
+    else
+     # @arr_text.join
+      accurate(text_size - @arr_text.join.bytesize)
+    end
+    #accurate(text_size - @arr_text.join.bytesize)
   end
 
   def string
@@ -35,5 +46,15 @@ class RandomText
     end
     arr[-1] = arr.last.rstrip
     arr.push(LS).join
+  end
+
+  def accurate(bytes)
+    puts "HERE!!!"
+    str = string
+    if (bytes - str.size).zero?
+      @arr_text.push(str).join
+    else
+      accurate(bytes)
+    end
   end
 end
