c7eb1b682acbfaadd6c7866dac2c5ea9c25f7981
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-08-04 22:44:05 +0300

    Lesson_19 tasks 1,2,3 completed

--- Lesson_19/lib/randomtext.rb
+++ Lesson_19/lib/randomtext.rb
@@ -5,56 +5,67 @@ class RandomText
   attr_accessor :text_size, :text
 
   MIN = 1
+  MIN_BYTES = 50
   MAX_CHARS = 12
   MAX_WORDS = 25
-  MAX_STRING = 25
   LS = ".\r\n"
   def initialize(text_size)
     @text_size = text_size
     @arr_text = []
     @text = gettext
-    
   end
 
   def gettext
-    rand(MIN..MAX_STRING).times do
-      line_string = string
-      return @arr_text.join unless (line_string.bytesize + @arr_text.join.bytesize) < text_size
-
+    line_string = string
+    if (line_string.bytesize + @arr_text.join.bytesize) < text_size
       @arr_text.push(line_string)
-      #puts "DEBUG1_before: #{text_size - @arr_text.join.bytesize}"
-    end
-    #puts text_size / 10
-    if (text_size - @arr_text.join.bytesize) > 100
-      puts "DEBUG_before: #{text_size - @arr_text.join.bytesize}"
-      gettext
-      puts "DEBUG_after: #{text_size - @arr_text.join.bytesize}"
+      first_correct_array
     else
-     # @arr_text.join
-      accurate(text_size - @arr_text.join.bytesize)
+      correct_array
     end
-    #accurate(text_size - @arr_text.join.bytesize)
+  end
+
+  def first_correct_array
+    if (text_size - @arr_text.join.bytesize) > MIN_BYTES
+      gettext
+    else
+      correct_array
+    end
+  end
+
+  def correct_array
+    accurate(text_size - @arr_text.join.bytesize)
   end
 
   def string
-    arr = []
+    arr_string = []
     rand(MIN..MAX_WORDS).times do
       word = ('0'..'z').to_a.sample(rand(MIN..MAX_CHARS)).join
-      return arr.push(LS).join unless (arr.join.bytesize + word.bytesize + LS.bytesize) < text_size
-
-      arr.push("#{word} ")
+      check_string_size(arr_string, word)
     end
-    arr[-1] = arr.last.rstrip
-    arr.push(LS).join
+    arr_string[-1] = arr_string.last.rstrip
+    arr_string.push(LS).join
+  end
+
+  def check_string_size(arr_string, word)
+    if (arr_string.join.bytesize + word.bytesize + LS.bytesize) < text_size
+      arr_string.push("#{word} ")
+    else
+      arr_string.push(LS).join
+    end
+    arr_string
   end
 
   def accurate(bytes)
-    puts "HERE!!!"
     str = string
     if (bytes - str.size).zero?
       @arr_text.push(str).join
     else
-      accurate(bytes)
+      begin
+        accurate(bytes)
+      rescue StandardError
+        @arr_text.join
+      end
     end
   end
 end
--- Lesson_19/new_file.rb
+++ Lesson_19/new_file.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 
+require 'fileutils'
 require_relative 'lib/randomtext'
 
-text_size = 1_000 # bytes
+puts `clear`
+puts "\nTask 2"
+text_size = 1000 # bytes
 name = 'some.txt'
+File.delete(name) if File.exist?(name)
 file = File.new(name, 'a')
 some_text = RandomText.new(text_size).text
-puts "Text size: #{some_text.bytesize} bytes"
-puts "Your condition max: #{text_size} bytes"
-puts 'Some text:'
-puts "\n"
-puts some_text
+puts "\nCreated random text, size: #{some_text.bytesize} bytes"
+puts "File size set: #{text_size} bytes"
+# puts 'Some text:'
+# puts "\n"
+# puts some_text
 file << some_text
+puts "File '#{name}' created. Size = #{file.size}"
+puts "\nTask 3"
+arr = File.readlines(file).map(&:chomp)
+# puts arr
+# puts "\n"
+max_str = arr.max { |a, b| a.size <=> b.size }
+min_str = arr.min { |a, b| a.size <=> b.size }
+puts "\nMax size: '#{max_str.size}' String:'#{max_str}'"
+puts "Min size: '#{min_str.size}' String:'#{min_str}'"
