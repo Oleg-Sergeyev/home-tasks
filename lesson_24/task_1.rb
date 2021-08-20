ed4c17ff950c7e1cffc5aa79e547ff29817c7bc4
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-08-20 10:38:12 +0300

    Lesson_24, added tasks 1,2

---
+++ lesson_24/task_1.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# task 1
+# num = rand(1..10)
+# p ((1..num).to_a).concat((1..num - 1).to_a.reverse)
+
+# task 2
+# matrix = [
+#   [1, 2, 3, 11],
+#   [4, 5, 6, 12],
+#   [7, 8, 9, 13],
+#   [7, 8, 9, 13]
+# ]
+# s = 0
+# matrix.each_with_index do |array, index|
+#   s += array[index]
+# end
+# puts s
+
+# task_3
+(1...9)
