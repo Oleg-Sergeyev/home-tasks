66cd969a05659f98b1e44f4499164fa231f9ffa9
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-08-24 00:33:29 +0300

    Codewars, added task 'Evaporator'

---
+++ Codewars/Evaporator/run.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+def evaporator(content, evap_per_day, threshold)
+  threshold_ml = (content.to_f * threshold) / 100
+  (1..100).reduce(content) do |sum, day|
+    return day unless (sum -= (evap_per_day * sum.to_f / 100)) > threshold_ml
+
+    sum
+  end
+end
+
+p evaporator(10, 10, 5)
+
+# Other solutions
+
+# def evaporator(content, percentage, threshold)
+#   Math.log(threshold/100.0, 1-percentage/100.0).ceil
+# end
+
+# def evaporator(c, e, t, o = c, s = 0)
+#   c < t*0.01*o ? s : evaporator(c*(1-0.01*e), e, t, o, -~s)
+# end
