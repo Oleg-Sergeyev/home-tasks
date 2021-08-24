f6a4623c8b0830f4627e847ed97dfdb1a3389f01
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-08-24 22:48:22 +0300

    Codewars, completed  task Decimal to Factorial and Back

--- Codewars/Decimal_to_Factorial_and_ Back/run.rb
+++ Codewars/Decimal_to_Factorial_and_ Back/run.rb
@@ -2,34 +2,33 @@
 
 def dec_2_fact_string(nb)
   arr = []
-  code = (0..9).to_a.concat(('A'..'Z').to_a)
-  #if nb >= 36288000
-  code.each_with_index.reduce(nb) do |memo, (num, index)|
-    p num
-    return (arr << memo.remainder(num + 1)).reverse.join if (memo / (num + 1)).zero?
+  nums = (0..9).to_a.concat(('A'..'Z').to_a)
+  nums.each_with_index.reduce(nb) do |memo, (_num, index)|
+    return (arr << nums.at(memo.remainder(index + 1))).reverse.join if (memo / (index + 1)).zero?
 
-    arr << (memo.remainder(num + 1).zero? ? 0 : memo.remainder(num + 1))
-    memo /= num + 1
+    arr << (memo.remainder(index + 1).zero? ? 0 : nums.at(memo.remainder(index + 1)))
+    memo /= index + 1
     memo
   end
 end
 
 def fact_string_2_dec(str)
-  str.to_i.digits.each_with_index.reduce(0) do |sum, (val, index)|
-    sum += ((1..index).reduce(1, :*) * val)
+  nums = (0..9).to_a.concat(('A'..'Z').to_a)
+  str.chars.reverse!.each_with_index.reduce(0) do |sum, (val, index)|
+    val = val.to_i if (48..57).include? val.ord
+    sum += ((1..index).reduce(1, :*) * nums.index(val))
     sum
   end
 end
 
-# 463 = 3×5! + 4×4! + 1×3! + 0×2! + 1×1! + 0×0!
-number = 3628799 # [0, 1, 0, 1, 4, 3]
+number = 463 # '341010'
 p dec_2_fact_string(number)
 
-string = '341010'
+string = 'ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210' # 371993326789901217467999448150835199999999
+# string = '341010' # 463
 p fact_string_2_dec(string)
 
-# number = 36288000
-# number = 371993326789901217467999448150835199999999
-# Expected: "A0000000000", instead got: "100000000000"
-# Expected: "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210", 
-# instead got: "3534333231302928272625242322212019181716151413121110987654321
+# number: 36288000 => "A0000000000"
+# number: 371993326789901217467999448150835199999999 => "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210"
+
+# 463 = 3×5! + 4×4! + 1×3! + 0×2! + 1×1! + 0×0!
