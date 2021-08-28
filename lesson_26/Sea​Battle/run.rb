da881358e0358e8bb95dc4c1067431924bf25d24
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-08-29 00:36:13 +0300

    SeaBattle, added testing code

--- lesson_26/Sea​Battle/run.rb
+++ lesson_26/Sea​Battle/run.rb
@@ -3,11 +3,16 @@
 require_relative 'lib/battlefield'
 require_relative 'lib/navy'
 
-map = BattleField.new
-puts `clear`
-
-ships = Navy.new([4, 3, 3, 2, 2, 2, 1, 1, 1, 1]) # fleet
-ships.fleet.each do |boat|
-  Navy.set_boats_on_map(boat, map)
+(1..nil).each do |num|
+  map = BattleField.new
+  puts `clear`
+  puts '      Sea Battle'
+  puts
+  ships = Navy.new([4, 3, 3, 2, 2, 2, 1, 1, 1, 1]) # fleet
+  ships.fleet.each do |boat|
+    Navy.set_boats_on_map(boat, map)
+  end
+  map.view
+  puts "        map №#{num}"
+  sleep 3
 end
-map.view
