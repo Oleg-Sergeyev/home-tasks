a83995f6d2ad38c8190188f40d8e3f91dae719c9
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-09-14 01:38:10 +0300

    Changed queries for Lesson_5

--- SQLite_db/Queries/Lesson_5/queries_lesson_5.sql
+++ SQLite_db/Queries/Lesson_5/queries_lesson_5.sql
@@ -10,11 +10,56 @@
     # значением и средней успеваемостью преподавателя с наименьшим значением. 
     # Средняя успеваемость считается по всем потокам преподавателя.
 
+    .tables
+    .header on
+    .mode column
+
     SELECT * FROM streams;
     SELECT * FROM courses;
+    SELECT * FROM teachers;
+    SELECT * FROM academic_perfomance;
 
+# task 1
     SELECT
       (SELECT number FROM streams WHERE students_amount >= 40) AS num,
       name,
       (SELECT students_amount FROM streams WHERE students_amount > 40) AS amount
     FROM courses;
+# task 2
+    SELECT
+      (SELECT number FROM streams WHERE id = academic_perfomance.stream_id) AS stream_number,
+      (SELECT name FROM courses WHERE academic_perfomance.stream_id = id) AS course_name,
+      (SELECT surname || ' ' || name FROM teachers WHERE id = academic_perfomance.teacher_id ) AS full_name,
+      perfomance
+    FROM academic_perfomance
+    GROUP BY perfomance HAVING MIN(perfomance) LIMIT 2;
+# task 3
+    SELECT
+      teacher_id,
+      AVG(perfomance) AS avg_perfomane
+    FROM academic_perfomance
+    WHERE teacher_id = (SELECT id FROM teachers WHERE name = 'Николай' AND surname = 'Савельев');
+# task 4
+    SELECT
+      (SELECT number FROM streams WHERE id = academic_perfomance.stream_id) AS stream_number,
+      (SELECT surname || ' ' || name FROM teachers WHERE id = academic_perfomance.teacher_id ) AS full_name,
+      perfomance
+    FROM academic_perfomance
+    WHERE teacher_id = (SELECT id FROM teachers WHERE name = 'Наталья' AND surname = 'Петрова')
+    UNION
+    SELECT
+      (SELECT number FROM streams WHERE id = academic_perfomance.stream_id) AS stream_number,
+      (SELECT surname || ' ' || name FROM teachers WHERE id = academic_perfomance.teacher_id ) AS full_name,
+      perfomance
+    FROM academic_perfomance
+    GROUP BY perfomance HAVING AVG(perfomance) < 4.8;
+# task 5
+      SELECT
+        surname || ' ' || name AS full_name,
+        (SELECT
+          (AVG(perfomance)) || ' : ' ||
+          (AVG(perfomance) - (SELECT MAX(perfomance) FROM academic_perfomance)) || ' : ' ||
+          (AVG(perfomance) - (SELECT MIN(perfomance) FROM academic_perfomance))
+        FROM academic_perfomance
+        WHERE teacher_id = id) AS 'avg : avg - max : avg - min'
+      FROM teachers;
