1fcf1c5f263027885fa13064d98ff3155b84592b
Oleg_Sergeyev <sergeyev_ov@mail.ru>
2021-09-14 23:47:59 +0300

    Updated remarks

--- SQLite_db/Queries/Lesson_7/queries_lesson_7.sql
+++ SQLite_db/Queries/Lesson_7/queries_lesson_7.sql
@@ -10,10 +10,33 @@
 .mode column
 
 SELECT * FROM streams;
+--id  number  started_at  students_amount  course_id  finished_at
+----  ------  ----------  ---------------  ---------  -----------
+--1   165     2020-08-18  34               3                     
+--2   178     2020-10-02  37               2                     
+--3   203     2020-12-11  35               1                     
+--4   210     2020-03-12  41               1           
 SELECT * FROM courses;
-SELECT * FROM teachers;
-SELECT * FROM academic_perfomance;
+--id  name                  
+----  ----------------------
+--1   Базы данных           
+--2   Основы Python         
+--3   Linux. Рабочая станция
 
+SELECT * FROM teachers;
+--id  name     surname   email                   email2
+----  -------  --------  ----------------------  ------
+--1   Николай  Савельев  saveliev.n@mai.ru             
+--2   Наталья  Петрова   petrova.n@yandex.ru           
+--3   Елена    Малышева  malisheva.e@google.com  
+SELECT * FROM academic_perfomance;
+--teacher_id  stream_id  perfomance
+------------  ---------  ----------
+--3           1.0        4.7       
+--2           2.0        4.9       
+--1           3.0        4.8       
+--1           4.0        4.9  
+    
 -- task 1
 CREATE VIEW last_course AS
   SELECT
